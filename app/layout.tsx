22926ea9aba928590ce00e53a2d0771d65829f32
camadavizual-cloud <camadavizual@gmail.com>
2026-09-01 17:45:30 -0300

    Adota tipografia Montserrat

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,5 +29,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="pt-BR"><body>{children}</body></html>;
+  return <html lang="pt-BR"><head><link rel="preconnect" href="https://fonts.googleapis.com" /><link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /><link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet" /></head><body>{children}</body></html>;
 }
